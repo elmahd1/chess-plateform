7d4023f65797b1735a8f21a9cc1e980b1c947ae2
elmahdi <elmhadrielmahdi@gmail.com>
2025-02-24 11:23:16 +0000

    changing the project

--- project-gesture/src/main/java/com/main/demo/MainApplication.java
+++ project-gesture/src/main/java/com/main/demo/MainApplication.java
@@ -1,13 +1,36 @@
 package com.main.demo;
-
+import com.main.demo.models.*;
+import com.main.demo.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication implements CommandLineRunner {
+	@Autowired
+	private UserRepo utilisateurRepository;	
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
+    @Override
+    public void run(String... args) {
+        // Ajouter un utilisateur dans la base de données
+        User user = new User();
+        user.setNom("Elmahdi");
+        user.setEmail("elmahdi@example.com");
+        UserRepo.save(user);
+        System.out.println("Utilisateur ajouté avec succès !");
 
+        // Récupérer tous les utilisateurs
+        List<User> utilisateurs = UserRepo.findAll();
+        utilisateurs.forEach(u -> System.out.println(u.getNom() + " - " + u.getEmail()));
+    }
 }
